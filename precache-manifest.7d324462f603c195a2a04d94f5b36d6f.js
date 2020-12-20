52fb50e040be6ac23030b4c426a7b969be1cda60
xqy547 <xqy547@users.noreply.github.com>
2020-12-20 06:11:12 +0000

    deploy: AMFEIX-DEVELOPMENT/AMFEIX@5596e6313ca408869156a930cb14ff45e87b2a33

---
+++ precache-manifest.7d324462f603c195a2a04d94f5b36d6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca89fbcf47f647ea3e322763a240801",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf3f48eb8bafc77c8e12",
+    "url": "/static/js/2.ff16d626.chunk.js"
+  },
+  {
+    "revision": "c88fbb3c06172888b1aad76fb74ef9bb",
+    "url": "/static/js/2.ff16d626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd59f779514016769121",
+    "url": "/static/js/main.8b8f8847.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
